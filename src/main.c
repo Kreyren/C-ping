43e9ee00acf4beda63f413ba5d03cd2407b5969b
Kreyren <KostWarCZE@RiXotStudio.cz>
2020-01-19 06:22:38 +0000

    init

---
+++ src/main.c
@@ -0,0 +1,7 @@
+// Created by Jacob Hrbek <kreyren@rixotstudio.cz> in 2020 under GPL-3 license (https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+#include <stdio.h>
+
+int main() {
+	printf("%s\n", "ping!");
+}
